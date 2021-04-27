189908b40248c2a72a7ec76a081b31bcf3b44c2d
lucianaechevarriaa <luciana.echevarria@normaltandil.edu.ar>
2021-04-27 00:00:45 -0300

    ultima modificacion chatbot

--- chatbot_instituto.py
+++ chatbot_instituto.py
@@ -13,16 +13,6 @@ from datetime import datetime
 from datetime import timedelta
 
 
-#from BD.pgdatabase import DB
-
-# Se aplican expresiones regulares para determinar lo que ingresa el/la usuario/a
-# (.*) representa cualquier cadena luego o antes de una palabra o frase.
-# Por ejemplo mi nombre es (.*) hace match con "mi nombre es Sam", "mi nombe es Mariano Moreno",
-# "mi nombre es Lucia pero me dice Lu"
-# Luego en la respuesta, la referencia a %1 indica la expresión que tenga (.*),
-# por ejemplo la respuesta a "mi nombre es Sam" sería "Hola Sam, como estas ?"
-
-
 class MyChat(Chat):
 
     conect = base.bbdd
@@ -30,8 +20,6 @@ class MyChat(Chat):
     etiquetas = {}
 
     def __init__(self, pairs, reflections={}):
-
-        # add `z` because now items in pairs have three elements
         self._pairs = [(re.compile(x), y, z) for (x, y, z) in pairs]
         self._reflections = reflections
         self._regex = self._compile_reflections()
@@ -40,19 +28,8 @@ class MyChat(Chat):
 
     def respond(self, str):
 
-        """
-        Redefinimos el método que genera la respuesta.
-        El objetivo es poder hacer el análisis de palabras.
-
-        :type str: str
-        :param str: Cadena a ser mapeada para dar respuesta
-        :rtype: str
-        """
-
         flag_match = True
         if(flag_match):
-            # desarma una cadena en tokens (palabras o  etiquetas) y uso un diccionario para
-            # contar ocurrencias.
             tokens = word_tokenize(str)
             for tok in tokens:
                 if tok not in self.etiquetas:
@@ -61,25 +38,18 @@ class MyChat(Chat):
                     self.etiquetas[tok] += 1
 
             flag_match = False
-
-        # Bucle que verifica si el parámetro hace match con alguna de las reglas.
-        # En base a esto elabora la respuesta y llama a una función si aplica.
-
         for (pattern, response, callback) in self._pairs:
             match = pattern.match(str)
 
             if match:
 
-                resp = random.choice(response)
-                #resp = self._wildcards(resp, match)
-                
+                resp = random.choice(response)                
                 if resp[-2:] == '?.':
                     resp = resp[:-2] + '.'
                 if resp[-2:] == '??':
                     resp = resp[:-2] + '?'
 
-                # run `callback` if exists
-                if callback: # eventually: if callable(callback):
+                if callback: 
                     var = str.split()[1]
                     menu = "\n\n****************************************************\nSI DESEA                                   INGRESE\nIr al menú principal docente               MENU_DOC\nIr al menú principal estudiante            MENU_EST\nIr al menú principal invitado              MENU_INV\nVolver al inicio                           INICIO\nSalir                                      SALIR\n****************************************************"
                     function = callback(match,var)
@@ -210,62 +180,76 @@ def get_datos_institucionales(match,p_dato):
         retorno = retorno +"Región: "+str(dato[0])+"- Distrito: "+str(dato[1])+"- Nombre: "+dato[2]+"- Establecimiento: "+dato[3]+"\n"
     return retorno
 
+mis_reflexions = {
+    "INICIO":"inicio",
+    "hola":["HOLA","buenas tardes","BUENAS TARDES","buenas noches","BUENAS NOCHES","buen día","buen dia","BUEN DIA"],
+    "estudiante":["ESTUDIANTE","alumno","ALUMNO","alumna","ALUMNA"],
+    "docente":["DOCENTE","profesor","PROFESOR","profesora","PROFESORA"],
+    "invitado":["INVITADO","interesado","INTERESADO"],
+    "nombre y apellido":["NOMBRE Y APELLIDO","apellido y nombre","APELLIDO Y NOMBRE"],
+    "CARRERA":"carrera",
+    "MATERIA":"materia",
+    "SALIR":["salir","chau","CHAU"],
+    "dni":["documento","DOCUMENTO","d.n.i.","D.N.I."],
+    "legajo":"LEGAJO",
+    "MATRICULACION":["matriculacion","matriculación","matricula","matrícula","MATRICULA"],
+    "NOTAS":"notas",
+    "ASISTENCIAS": "asistencias"}
 
 pares = [
-    [   r"INICIO",
+    [   r"INICIO|inicio|hola|(.*)hola|(.*)hola(.*)|hola(.*)",
         ["Hola soy un bot, eres estudiante, docente o invitado?"],None
     ],
-
     [   r"(.*)docente|docente(.*)|(.*)docente(.*)|docente",
         ["\nIngrese la letra 'L'+ espacio, seguido de su número de legajo"],None
     ],
     [
-        r"(L [0-9]{1,2})",
-        ["\nHola {}. Si desea saber\nACERCA DE                  INGRESE\nInformación general	     INFO\nmaterias                   MATERIA\ncarreras                   CARRERA\ndatos de docentes          DDOC\ndatos de estudiantes       DEST\n"],
+        r"(L [0-9]{1,2}|l [0-9]{1,2})",
+        ["\nHola {}. Si desea saber\nACERCA DE                  INGRESE\nInformación general	   INFO\nmaterias                   MATERIA\ncarreras                   CARRERA\ndatos de docentes          DDOC\ndatos de estudiantes       DEST\n"],
         get_nomape_docente
     ],
     [
-        r"MENU_DOC",
+        r"MENU_DOC|menu_doc",
         ["\nSi desea saber\nACERCA DE                  INGRESE\nInformación general	   INFO\nMaterias                   MATERIA\nCarreras                   CARRERA\nDatos de docentes          DDOC\nDatos de estudiantes       DEST\n"],None
     ],
-    [   r"MATERIA",
+    [   r"MATERIA|(.*)MATERIA|MATERIA(.*)|(.*)MATERIA(.*)",
         ["\nIngrese la letra 'M'+ espacio, seguido el nombre de la materia."],None
 
     ],
-    [   r"M (.*)",
+    [   r"M (.*)|m (.*)",
         ["\n{}"],
         get_datos_materias
     ],
-    [   r"CARRERA",
+    [   r"CARRERA|(.*)CARRERA|CARRERA(.*)|(.*)CARRERA(.*)",
         ["\nCARRERA: Profesorado de educacion inicial\nSi desea saber        \nACERCA DE       INGRESE        \nMaterias        CM+espacio+I        \nTitulo          CT+espacio+I        \nNivel           CN+espacio+I        \n\nCARRERA: Profesorado de educacion especial\nSi desea saber        \nACERCA DE       INGRESE        \nMaterias        CM+espacio+E        \nTitulo          CT+espacio+E        \nNivel           CN+espacio+E        \n\nCARRERA: Tecnicatura superior en analisis desarrollo y programacion de aplicaciones\nSi desea saber        \nACERCA DE       INGRESE        \nMaterias        CM+espacio+A        \nTitulo          CT+espacio+A        \nNivel           CN+espacio+A\n"],None
     ],
-    [   r"CT (.*)",
+    [   r"CT (.*)|ct (.*)",
         ["\n{}\n\n****************************************************\nSi desea volver al menú anterior ingrese CARRERA"],
         get_titulo_carrera
     ],
-    [   r"CN (.*)",
+    [   r"CN (.*)|cn (.*)",
         ["\n{}\n\n****************************************************\nSi desea volver al menú anterior ingrese CARRERA"],
         get_nivel_carrera
     ],
-    [   r"CM (.*)",
+    [   r"CM (.*)|cm (.*)",
         ["\n{}\n\n****************************************************\nSi desea volver al menú anterior ingrese CARRERA"],
         get_materias_carreras
     ],
-    [   r"DDOC",
+    [   r"DDOC|ddoc",
         ["\nIngrese el DD + espacio seguido del número de legajo del docente\n"],None
     ],
-    [   r"(DD [0-9]{1,2})",
+    [   r"(DD [0-9]{1,2}|dd [0-9]{1,2})",
         ["\n{}"],
         get_datos_docentes_por_legajo
     ],
-    [   r"NDOC (.*)",
+    [   r"NDOC (.*)|ndoc (.*)",
         ["\n{}"],
         get_datos_docentes_por_nombre
     ],
-    [   r"DEST",
+    [   r"DEST|dest",
         ["\nIngrese el DE + espacio seguido del número de legajo del estudiante\n"],None
     ],
-    [   r"(DE [0-9]{1,2})",
+    [   r"(DE [0-9]{1,2}|de [0-9]{1,2})",
         ["\n{}"],
         get_datos_estudiantes
     ],
@@ -273,94 +257,82 @@ pares = [
     [   r"(.*)estudiante|estudiante(.*)|(.*)estudiante(.*)|estudiante",
         ["\nIngrese 'DNI'+ espacio, seguido de su número de dni"],None
     ],
-    [   r"(DNI [0-9]{8})",
-        ["\nHola {}.\nSi desea saber\nACERCA DE                  INGRESE\nInformación general	   INFO\nmatriculación		   MATRICULACION\nnotas			   NOTAS\nasistencia		   ASISTENCIAS"],
+    [   r"(DNI [0-9]{8}|dni [0-9]{8})",
+        ["\nHola {}.\nSi desea saber\nACERCA DE                  INGRESE\nInformación general	   INFO\nMatriculación		   MATRICULACION\nNotas			   NOTAS\nAsistencia		   ASISTENCIAS"],
         get_nomape_estudiante
     ],
-    [   r"MENU_EST",
+    [   r"MENU_EST|menu_est",
         ["\nSi desea saber\nACERCA DE                  INGRESE\nInformación general	   INFO\nMatriculación		   MATRICULACION\nNotas			   NOTAS\nAsistencia		   ASISTENCIAS"],None
     ],
     [   r"legajo|(.*)legajo|legajo(.*)|(.*)legajo(.*)",
         ["\nSI ES          INGRESE        \nEstudiante     LEG_EST + espacio + Nro de DNI        \nDocente        LEG_DOC + espacio + Nro de DNI"],None
     ],
-    [   r"(LEG_EST [0-9]{8})",
+    [   r"(LEG_EST [0-9]{8}|leg_est [0-9]{8})",
         ["\nTu número de legajo es {}."],
         get_legajo_estudiante
     ],
-    [   r"(LEG_DOC [0-9]{8})",
+    [   r"(LEG_DOC [0-9]{8}|leg_doc [0-9]{8})",
         ["\nTu número de legajo es {}."],
         get_legajo_docente
     ],
-    [   r"MATRICULACION",
+    [   r"MATRICULACION|(.*)MATRICULACION|MATRICULACION(.*)|(.*)MATRICULACION(.*)",
         ["\nIngrese MMAT + espacio + Nro de legajo"],None
     ],
-    [   r"(MMAT [0-9]{1,2})",
+    [   r"(MMAT [0-9]{1,2}|mmat [0-9]{1,2})",
         ["\n{}"],
         get_matriculas_estudiantes
     ],
-    [   r"NOTAS",
+    [   r"NOTAS|(.*)NOTAS|NOTAS(.*)|(.*)NOTAS(.*)",
         ["\nSi desea saber\nACERCA DE                INGRESE\nNotas aprobadas	         VMNOT + espacio + Nro de legajo\nNotas desaprobadas	 FMNOT + espacio + Nro de legajo\nTodas las notas	         MNOT + espacio + Nro de legajo"],None
     ],
-    [   r"(MNOT [0-9]{1,2})",
+    [   r"(MNOT [0-9]{1,2}|mnot [0-9]{1,2})",
         ["\n{}\n\n****************************************************\nSi desea volver al menú anterior ingrese NOTAS"],
         get_notas_estudiantes
     ],
-        [r"(VMNOT [0-9]{1,2})",
+        [r"(VMNOT [0-9]{1,2}|vmnot [0-9]{1,2})",
         ["\n{}\n\n****************************************************\nSi desea volver al menú anterior ingrese NOTAS"],
         get_notas_aprobadas_estudiantes
     ],
-        [r"(FMNOT [0-9]{1,2})",
+        [r"(FMNOT [0-9]{1,2}|fmnot [0-9]{1,2})",
           ["\n{}\n\n****************************************************\nSi desea volver al menú anterior ingrese NOTAS"],
         get_notas_desaprobadas_estudiantes
     ],
-    [   r"ASISTENCIAS",
+    [   r"ASISTENCIAS|ASISTENCIAS|(.*)ASISTENCIAS|(.*)ASISTENCIAS(.*)|ASISTENCIAS(.*)",
         ["\nSi desea saber        \nACERCA DE                INGRESE\nPresencias     	         VMASIS + espacio + Nro de legajo\nAusencias                FMASIS + espacio + Nro de legajo\nTodas las asistencias    MASIS + espacio + Nro de legajo"],None
     ],
-    [   r"(FMASIS [0-9]{1,2})",
+    [   r"(FMASIS [0-9]{1,2}|fmasis [0-9]{1,2})",
         ["\n{}\n\n****************************************************\nSi desea volver al menú anterior ingrese ASISTENCIAS"],
         get_ausencia_estudiantes
     ],
-    [   r"(VMASIS [0-9]{1,2})",
+    [   r"(VMASIS [0-9]{1,2}|vmasis [0-9]{1,2})",
         ["\n{}\n\n****************************************************\nSi desea volver al menú anterior ingrese ASISTENCIAS"],
         get_presencia_estudiantes
     ],
-    [   r"(MASIS [0-9]{1,2})",
+    [   r"(MASIS [0-9]{1,2}|masis [0-9]{1,2})",
         ["\n{}\n\n****************************************************\nSi desea volver al menú anterior ingrese ASISTENCIAS"],
         get_asistencia_estudiantes
     ],
     [   r"(.*)invitado|invitado(.*)|(.*)invitado(.*)|invitado|MENU_INV",
         ["\nHola. Si desea saber\nACERCA DE                  INGRESE\nInformación general	   INFO\nMaterias                   MATERIA\nCarreras                   CARRERA\nDatos de docentes          NDOC + espacio + nombre del docente"],None
     ],
-    [   r"INFO",
+    [   r"INFO|info|(.*)info|(.*)info(.*)|info(.*)",
         ["\nSi desea información\nACERCA DE                  INGRESE\nDatos de contacto          DATOS + espacio + INFO\nDatos institucionales      INST + espacio + INFO"],None
     ],
-    [   r"DATOS (.*)",
+    [   r"DATOS (.*)|datos (.*)",
         ["\n{}\n\n****************************************************\nSi desea volver al menú anterior ingrese INFO"],
         get_datos_institucionales_contacto
     ],
-    [   r"INST (.*)",
+    [   r"INST (.*)|inst (.*)",
         ["\n{}\n\n****************************************************\nSi desea volver al menú anterior ingrese INFO"],
         get_datos_institucionales
+    ],
+    [   r"SALIR|(.*)SALIR|SALIR(.*)|(.*)SALIR(.*)",
+        ["\nHasta luego. Quedamos a su disposición!"],None   
+
     ],
 
 
 ]
-mis_reflexions = {
-    "ir": "fui",
-    "hola": "hey",
-    "docente":"profesor",
-    "estudiante":"alumno",
-    "particular":"interesado",
-    "nombre y apellido":"apellido y nombre",
-    "nombre":"apellido",
-    "dni":"documento",
-    "asistencia":"asistencias",
-    "carrera":"carreras",
-    "materia":"materias",
-    "datos de docente":"datos de docentes",
-    "datos de docente":"datos docente",
-    "datos de docente":"datos docentes"
-}
 
 def chatear():
     print("Hola soy un bot, eres estudiante, docente o invitado?")
